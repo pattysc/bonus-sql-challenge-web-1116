904ddae3bc06ebd1252609ebf224d730b20a8ab2
pattysc <pattys@users.noreply.github.com>
2016-12-11 13:04:39 -0500

    Done.

--- answers.sql
+++ answers.sql
@@ -1,1 +1,59 @@
 -- Write your queries here!
+question_one = <<-SQL
+  SELECT COUNT(guestlist)
+  FROM dailyshow
+  GROUP BY guestlist
+  ORDER BY COUNT(guestlist) ASC;
+SQL
+answer: Fareed Zakaria, 19 times
+
+question_two = <<-SQL
+SELECT occupation, COUNT(occupation)
+FROM dailyshow
+GROUP BY year
+ORDER BY year ASC;
+SQL
+
+question_three = <<-SQL
+SELECT occupation, COUNT(occupation)
+FROM dailyshow
+GROUP BY occupation
+ORDER BY COUNT(occupation) ASC;
+SQL
+
+answer: actor, 596 times
+
+question_four = <<-SQL
+SELECT COUNT(guestlist)
+FROM dailyshow
+WHERE guestlist LIKE "Bill%";
+SQL
+
+answer: 56
+
+question_five = <<-SQL
+  SELECT showdate
+  FROM dailyshow
+  WHERE guestlist = "Patrick Stewart"
+SQL
+
+answer:
+4/12/00
+4/21/03
+11/7/13
+
+question_six = <<-SQL
+  SELECT year, COUNT(guestlist)
+  FROM dailyshow
+  GROUP BY year
+  ORDER BY COUNT(guestlist) ASC;
+SQL
+
+answer: 2000, 169 guestlist
+
+question_seven = <<-SQL
+  SELECT showg, COUNT(showg)
+  FROM dailyshow
+  GROUP BY year
+  ORDER BY COUNT(showg) ASC;
+SQL
--- parse.rb
+++ parse.rb
@@ -1,1 +1,73 @@
-# parse the CSV into a sqlite3 database here!
+require "sqlite3"
+require "csv"
+require 'pry'
+
+db = SQLite3::Database.new ":memory:"
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dailyshow (
+    id INTEGER PRIMARY KEY,
+    year INTEGER,
+    occupation TEXT,
+    showdate TEXT,
+    showg TEXT,
+    guestlist TEXT
+  )
+SQL
+
+rows = db.execute(sql)
+
+CSV.foreach("daily_show_guests.csv") do |row|
+  db.execute("INSERT INTO dailyshow (year, occupation, showdate, showg, guestlist) VALUES (?, ?, ?, ?, ?)", row[0], row[1], row[2], row[3], row[4])
+end
+
+question_one = <<-SQL
+  SELECT guestlist, COUNT(guestlist)
+  FROM dailyshow
+  GROUP BY guestlist
+  ORDER BY COUNT(guestlist) ASC;
+SQL
+
+question_two = <<-SQL
+SELECT occupation, COUNT(occupation)
+FROM dailyshow
+GROUP BY year
+ORDER BY year ASC;
+SQL
+
+question_three = <<-SQL
+SELECT occupation, COUNT(occupation)
+FROM dailyshow
+GROUP BY occupation
+ORDER BY COUNT(occupation) ASC;
+SQL
+
+question_four = <<-SQL
+SELECT COUNT(guestlist)
+FROM dailyshow
+WHERE guestlist LIKE "Bill%";
+SQL
+
+question_five = <<-SQL
+  SELECT showdate
+  FROM dailyshow
+  WHERE guestlist = "Patrick Stewart"
+SQL
+
+question_six = <<-SQL
+  SELECT year, COUNT(guestlist)
+  FROM dailyshow
+  GROUP BY year
+  ORDER BY COUNT(guestlist) ASC;
+SQL
+
+question_seven = <<-SQL
+  SELECT showg, COUNT(showg)
+  FROM dailyshow
+  GROUP BY year
+  ORDER BY COUNT(showg) ASC;
+SQL
+
+
+lol = db.execute(question_seven)
+puts lol
